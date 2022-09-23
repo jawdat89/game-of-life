b070cd938d47432b80848efcdf8522b0898a3dcb
jawda <jawdat.abdullah@innovisec.com>
2022-09-23 11:13:09 +0300

    bug fix and code refactoring

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -1,25 +1,20 @@
 // #region global variables
-let grid; // array[x][y]
-let resolution = 10;
-let cols; // as x
-let rows; // as y
-let canvas;
 // #endregion global variables
 
 // #region game functions
 // function for getting two demitional array of nulls
-const generateAndFillTwoDimintionalArray = (colsNumber, rowsNumber, maxNumber) => {
+const generateAndFillTwoDimintionalArray = (colsNumber, rowsNumber) => {
   const arr = new Array(colsNumber);
   for (let i = 0; i < colsNumber; i++) {
     arr[i] = new Array(rowsNumber);
     for (let j = 0; j < rowsNumber; j++) {
-      arr[i][j] = getRandomInt(maxNumber);
+      arr[i][j] = Math.floor(Math.random() * 2);
     }
   }
   return arr;
 }
 
-const countLiveNeighbors = (arr, x, y) => {
+const countLiveNeighbors = (arr, x, y, cols, rows) => {
   let sum = 0;
   for (let i = -1; i < 2; i++) {
     for (let j = -1; j < 2; j++) {
@@ -37,9 +32,11 @@ const countLiveNeighbors = (arr, x, y) => {
 }
 
 
-const draw = () => {
+const draw = ({ canvasId, grid, resolution, cols, rows }) => {
+
+  const canvas = document.querySelector(canvasId);
   if (canvas.getContext) {
-    let notClicked = true;
+    let notClicked = true; // causes Side Effect todo: remove
 
     let renderer = setInterval(() => {
       let ctx = canvas.getContext('2d');
@@ -47,12 +44,13 @@ const draw = () => {
         for (let j = 0; j < rows; j++) {
           let x = i * resolution;
           let y = j * resolution;
-          if (grid[i][j] == 1) {
+          if (grid[i][j] === 1) {
             ctx.fillStyle = "black";
-            ctx.strokeStyle = "white";
+            ctx.strokeStyle = "black";
             ctx.strokeRect(x, y, resolution, resolution);
             ctx.fillRect(x, y, resolution - 0.5, resolution - 0.5);
           } else {
+            ctx.fillStyle = "white";
             ctx.strokeStyle = "white";
             ctx.strokeRect(x, y, resolution, resolution);
           }
@@ -60,29 +58,33 @@ const draw = () => {
       }
 
 
-      let next = generateAndFillTwoDimintionalArray(cols, rows, 2);
+      let next = grid;
 
       for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
           let state = grid[i][j];
 
           // count live neighbors
-          const neighbors = countLiveNeighbors(grid, i, j);
+          const neighbors = countLiveNeighbors(grid, i, j, cols, rows);
 
           if (state === 1 && (neighbors === 2 || neighbors === 3)) {
             // Rule # 1
+            // Survival: Each live cell with either two or three live neighbors will remain alive for the next generation. 
             next[i][j] = 1;
           } else if (state === 0 && neighbors === 3) {
             // Rule # 2
+            // Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
             next[i][j] = 1;
           } else {
+            // Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
+            // Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation. 
             next[i][j] = 0;
           }
         }
       }
 
       grid = next;
-    }, 250);
+    }, 100);
 
     canvas.addEventListener("click", () => {
       notClicked = false;
@@ -91,33 +93,35 @@ const draw = () => {
   }
 }
 
-const setup = () => {
+const setup = ({ canvasId, resolution }) => {
   // canvas
-  canvas = document.querySelector('#canvas');
+  const canvas = document.querySelector(canvasId);
   const width = canvas.width;
   const height = canvas.height;
-  cols = Math.floor(width / resolution); // as x
-  rows = Math.floor(height / resolution); // as y
+  const cols = Math.floor(width / resolution); // as x
+  const rows = Math.floor(height / resolution); // as y
   // create grid of rows and cols and fill it with randoms of (0,1)
-  grid = generateAndFillTwoDimintionalArray(cols, rows, 2);
+  const grid = generateAndFillTwoDimintionalArray(cols, rows);
+
+  return {
+    grid,
+    cols,
+    rows
+  };
 }
 // #endregion game functions
 
-
-
 const init = () => {
-  setup();
-  let renderer = draw();
-  document.addEventListener('keyup', (e) => {
-    grid = generateAndFillTwoDimintionalArray(cols, rows, 2);
-  });
+  let grid; // array[x][y]
+  let resolution = 5;
+  let cols; // as x
+  let rows; // as y
+
+  const setupElements = setup({ canvasId: '#canvas', resolution });
+  grid = setupElements.grid;
+  cols = setupElements.cols;
+  rows = setupElements.rows;
+  draw({ canvasId: '#canvas', grid, resolution, cols, rows });
 }
 
 document.addEventListener("DOMContentLoaded", init);
-
-
-// #region methods
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
-}
-// #endregion methods
